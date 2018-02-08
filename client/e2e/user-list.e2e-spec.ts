b522e7b068a8ae3311283a98d5d486839779be63
carav008 <carav008@morris.umn.edu>
2018-02-08 11:30:28 -0600

    Update corresponding End to end testing for new layout

--- client/e2e/user-list.e2e-spec.ts
+++ client/e2e/user-list.e2e-spec.ts
@@ -17,7 +17,7 @@ browser.driver.controlFlow().execute = function () {
     return origFn.apply(browser.driver.controlFlow(), args);
 };
 
-describe('User list', () => {
+describe('angular-spark-lab', () => {
     let page: UserPage;
 
     beforeEach(() => {
@@ -26,27 +26,28 @@ describe('User list', () => {
 
     it('should get and highlight User Name attribute ', () => {
         page.navigateTo();
-        expect(page.getUserTitle()).toEqual('User Name');
+        expect(page.getUserTitle()).toEqual('Users');
     });
 
     it('should type something in filter name box and check that it returned correct element', () => {
         page.navigateTo();
-        page.typeAName("Lynn");
-        expect(page.getFirstUser()).toEqual("Lynn Ferguson is 25 years old");
+        page.typeAName("t");
+        expect(page.getUniqueUser("kittypage@surelogic.com")).toEqual("Kitty Page");
+        page.backspace();
+        page.typeAName("lynn")
+        expect(page.getUniqueUser("lynnferguson@niquent.com")).toEqual("Lynn Ferguson");
     });
 
-    it('should click on the age 27 times and return 3 elements then ', () => {
+    it('should click on the age 27 times and return 3 elements', () => {
         page.navigateTo();
         page.getUserByAge();
         for (let i = 0; i < 27; i++) {
             page.selectUpKey();
         }
 
-        expect(page.getFirstUser()).toEqual("Stokes Clayton is 27 years old");
+        expect(page.getUniqueUser("stokesclayton@momentia.com")).toEqual("Stokes Clayton");
 
-        page.typeAName("Merrill");
-
-        expect(page.getFirstUser()).toEqual("Merrill Parker is 27 years old");
+        expect(page.getUniqueUser("merrillparker@escenta.com")).toEqual("Merrill Parker");
 
     });
 });
