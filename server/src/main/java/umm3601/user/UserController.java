1e9bc27f95b0fb508fb3075ff0e1902c215e5a69
Nic McPhee <mcphee@morris.umn.edu>
2019-03-22 19:17:20 -0500

    Revert "Delete dangling Java code"
    This reverts commit b1180ed3. We actually need these Java files or the
    `User` part of the server won't run and the E2E tests won't pass. All
    that the JAR file provides that we need is the `TodoController`;
    everything else is in these files.

---
+++ server/src/main/java/umm3601/user/UserController.java
@@ -0,0 +1,72 @@
+package umm3601.user;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import spark.Request;
+import spark.Response;
+
+import java.io.IOException;
+
+import static umm3601.Util.*;
+
+/**
+ * Controller that manages requests for info about users.
+ */
+public class UserController {
+
+  private final Gson gson;
+  private Database database;
+
+  /**
+   * Construct a controller for users.
+   * <p>
+   * This loads the "database" of user info from a JSON file and
+   * stores that internally so that (subsets of) users can be returned
+   * in response to requests.
+   *
+   * @param database the database containing user data
+   */
+  public UserController(Database database) {
+    gson = new Gson();
+    this.database = database;
+  }
+
+  /**
+   * Get the single user specified by the `id` parameter in the request.
+   *
+   * @param req the HTTP request
+   * @param res the HTTP response
+   * @return a success JSON object if the user with that ID is found, a fail
+   * JSON object if no user with that ID is found
+   */
+  public JsonElement getUser(Request req, Response res) {
+    res.type("application/json");
+    String id = req.params("id");
+    User user = database.getUser(id);
+    if (user != null) {
+
+      //return buildSuccessJsonResponse("user", gson.toJsonTree(user));
+      return gson.toJsonTree(user);
+    } else {
+      String message = "User with ID " + id + " wasn't found.";
+      return buildFailJsonResponse("id", message);
+    }
+  }
+
+  /**
+   * Get a JSON response with a list of all the users in the "database".
+   *
+   * @param req the HTTP request
+   * @param res the HTTP response
+   * @return a success JSON object containing all the users
+   */
+  public JsonElement getUsers(Request req, Response res) {
+    res.type("application/json");
+    User[] users = database.listUsers(req.queryMap().toMap());
+
+    //return buildSuccessJsonResponse("users", gson.toJsonTree(users));
+    return gson.toJsonTree(users);
+  }
+
+}
