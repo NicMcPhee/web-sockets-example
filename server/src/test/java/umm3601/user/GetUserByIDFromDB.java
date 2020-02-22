207af038f5de284d44ed7869b12071018edaaafb
Waller Li <lsw626313@gmail.com>
2020-02-21 18:44:12 -0600

    Revert "Disable server tests to be updated with mongoDB"
    This reverts commit 9767b07f216fa1019682e33c642fd7a00154f1be.

--- server/src/test/java/umm3601/user/GetUserByIDFromDB.java
+++ server/src/test/java/umm3601/user/GetUserByIDFromDB.java
@@ -9,19 +9,19 @@ import org.junit.jupiter.api.Test;
 /**
  * Tests umm3601.user.Database getUser functionality
  */
-// public class GetUserByIDFromDB {
+public class GetUserByIDFromDB {
 
-//   @Test
-//   public void getStokesClayton() throws IOException {
-//     UserDatabase db = new UserDatabase("/users.json");
-//     User user = db.getUser("588935f52787254123f71fed");
-//     assertEquals("Stokes Clayton", user.name, "Incorrect name");
-//   }
+  @Test
+  public void getStokesClayton() throws IOException {
+    UserDatabase db = new UserDatabase("/users.json");
+    User user = db.getUser("588935f52787254123f71fed");
+    assertEquals("Stokes Clayton", user.name, "Incorrect name");
+  }
 
-//   @Test
-//   public void getBoltonMonroe() throws IOException {
-//     UserDatabase db = new UserDatabase("/users.json");
-//     User user = db.getUser("588935f5556f992bf8f37c01");
-//     assertEquals("Bolton Monroe", user.name, "Incorrect name");
-//   }
-// }
+  @Test
+  public void getBoltonMonroe() throws IOException {
+    UserDatabase db = new UserDatabase("/users.json");
+    User user = db.getUser("588935f5556f992bf8f37c01");
+    assertEquals("Bolton Monroe", user.name, "Incorrect name");
+  }
+}
