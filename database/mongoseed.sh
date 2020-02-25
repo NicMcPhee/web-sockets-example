b137ddcdbcbd16f5c96d599ab15ab188c029ff98
Paul Friederichsen <floogulinc@gmail.com>
2020-02-24 19:33:55 -0600

    Add dropping the database to mongoseed.sh

--- database/mongoseed.sh
+++ database/mongoseed.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+echo Dropping DB ${1:-dev}
+mongo ${1:-dev} --eval "db.dropDatabase()"
 for file in ./seed/*.json; do
   echo Seeding $(basename "$file" ".json") from $file in DB ${1:-dev}
   mongoimport --db=${1:-dev} --collection=$(basename "$file" ".json") --file=$file --jsonArray
