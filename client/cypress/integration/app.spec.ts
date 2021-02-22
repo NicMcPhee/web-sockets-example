bc407d559077a23c2989f498fb0bb6a5256413c2
Paul Friederichsen <floogulinc@gmail.com>
2021-02-22 17:17:36 -0600

    Remove redundant assertions in app.spec.ts

--- client/cypress/integration/app.spec.ts
+++ client/cypress/integration/app.spec.ts
@@ -12,25 +12,22 @@ describe('App', () => {
   it('The sidenav should open, navigate to "Users" and back to "Home"', () => {
     // Before clicking on the button, the sidenav should be hidden
     page.getSidenav()
-      .should('be.hidden')
-      .and('not.be.visible');
+      .should('be.hidden');
+
 
     page.getSidenavButton().click()
-      .should('not.be.hidden')
-      .and('be.visible');
+      .should('be.visible');
 
     page.getNavLink('Users').click();
     cy.url().should('match', /\/users$/);
     page.getSidenav()
-      .should('be.hidden')
-      .and('not.be.visible');
+      .should('be.hidden');
 
     page.getSidenavButton().click();
     page.getNavLink('Home').click();
     cy.url().should('match', /^https?:\/\/[^\/]+\/?$/);
     page.getSidenav()
-      .should('be.hidden')
-      .and('not.be.visible');
+      .should('be.hidden');
   });
 
 });
