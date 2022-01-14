b66bc45338d36cb91f0d80ea1fcb309850d5158e
Nic McPhee <mcphee@morris.umn.edu>
2022-01-14 00:52:24 -0600

    Add comments explaining warning suppression in test code
    I suppressed all the "Magic Number" Checkstyle warnings in both test files, but I forgot to include a comment to explain *why* I'd done that.

--- server/src/test/java/umm3601/user/UserControllerSpec.java
+++ server/src/test/java/umm3601/user/UserControllerSpec.java
@@ -42,6 +42,13 @@ import io.javalin.plugin.json.JavalinJson;
  *
  * @throws IOException
  */
+// The tests here include a ton of "magic numbers" (numeric constants).
+// It wasn't clear to me that giving all of them names would actually
+// help things. The fact that it wasn't obvious what to call some
+// of them says a lot. Maybe what this ultimately means is that
+// these tests can/should be restructured so the constants (there are
+// also a lot of "magic strings" that Checkstyle doesn't actually
+// flag as a problem) make more sense.
 @SuppressWarnings({ "MagicNumber" })
 public class UserControllerSpec {
 
