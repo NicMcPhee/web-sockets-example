2a09ab1dc93ee2a5c3c9cd94cdae1c68e3acc47b
K.K. Lamberty <lamberty@morris.umn.edu>
2023-02-22 19:07:50 -0600

    Use double quote to prevent globbing and word splitting
    Quoting variables prevents word splitting and glob expansion, and prevents the script from breaking when input contains spaces, line feeds, glob characters and such.
    
    Fix as suggested by Deep Source.

--- database/mongoseed.sh
+++ database/mongoseed.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 seed_db="${MONGO_DB:-dev}"
-echo Dropping DB $seed_db
+echo "Dropping DB $seed_db"
 mongo "$seed_db" --eval "db.dropDatabase()"
 for file in "$(dirname "$BASH_SOURCE")"/seed/*.json; do
   if [[ -f "$file" ]]; then
-    echo Seeding $(basename "$file" ".json") from $file in DB $seed_db
+    echo "Seeding $(basename "$file" ".json") from $file in DB $seed_db"
     mongoimport --db="$seed_db" --collection="$(basename "$file" ".json")" --file="$file" --jsonArray
   fi
 done
