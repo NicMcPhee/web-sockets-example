1f54219db404c87d1d95e0cd74c226846229de57
K.K. Lamberty <lamberty@morris.umn.edu>
2023-02-22 23:10:34 -0600

    Make intention explicit
    Having an empty body might seem like an accident, but in this case it's not. Having a non-empty body here helps us be sure that it's not empty by mistake.

--- client/src/app/users/user-card.component.ts
+++ client/src/app/users/user-card.component.ts
@@ -12,6 +12,7 @@ export class UserCardComponent implements OnInit {
   @Input() simple ? = false;
 
   ngOnInit(): void {
+    // do nothing, but the presence of this function makes other things work
   }
 
 }
