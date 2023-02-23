329563b89c01b2a0e4cdfb24de8ac420e3a8b033
K.K. Lamberty <lamberty@morris.umn.edu>
2023-02-23 00:33:37 -0600

    Figure out how to use flags for Deep Source

--- client/src/app/users/user-card.component.ts
+++ client/src/app/users/user-card.component.ts
@@ -11,9 +11,8 @@ export class UserCardComponent implements OnInit {
   @Input() user: User;
   @Input() simple ? = false;
 
-  //# skip qc JS-0604
   ngOnInit(): void {
-    // do nothing, but the presence of this function makes other things work
+    // do nothing, but the presence of this function makes other things work, skip qc JS-0604
   }
 
 }
