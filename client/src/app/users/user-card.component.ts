5f015c8ad7c14d6089ddc5c98009fa1f5bb26347
K.K. Lamberty <lamberty@morris.umn.edu>
2023-02-23 00:29:35 -0600

    Add flag to skip checking for an empty life cycle method here

--- client/src/app/users/user-card.component.ts
+++ client/src/app/users/user-card.component.ts
@@ -11,6 +11,7 @@ export class UserCardComponent implements OnInit {
   @Input() user: User;
   @Input() simple ? = false;
 
+  //# skip qc JS-0604
   ngOnInit(): void {
     // do nothing, but the presence of this function makes other things work
   }
