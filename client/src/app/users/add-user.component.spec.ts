927e856008b0bf5cf779f076044dd2379f5d550c
Nic McPhee <mcphee@morris.umn.edu>
2023-12-16 13:06:19 -0600

    Add tests for `AddUserComponent#submitForm`
    This adds tests for both the success and error paths for `AddUserComponent#submitForm`.
    
    This gets the test coverage for `AddUserComponent` to 100%, which is pretty nifty.
    
    The initial version of this code was suggested by GitHub CoPilot, but I had to make several changes to get it to compile and pass. I also added a variety of comments that hopefully helps explain what's going on.

--- client/src/app/users/add-user.component.spec.ts
+++ client/src/app/users/add-user.component.spec.ts
@@ -1,14 +1,19 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule, FormGroup, AbstractControl } from '@angular/forms';
+import { Location } from '@angular/common';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed, fakeAsync, flush, tick, waitForAsync } from '@angular/core/testing';
+import { AbstractControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
 import { MockUserService } from 'src/testing/user.service.mock';
 import { AddUserComponent } from './add-user.component';
+import { UserProfileComponent } from './user-profile.component';
 import { UserService } from './user.service';
 
 describe('AddUserComponent', () => {
@@ -259,4 +264,117 @@ describe('AddUserComponent', () => {
       expect(addUserComponent.getErrorMessage(controlName)).toEqual('Unknown error');
     });
   })
+});
+
+describe('AddUserComponent#submitForm()', () => {
+  let component: AddUserComponent;
+  let fixture: ComponentFixture<AddUserComponent>;
+  let userService: UserService;
+  let httpTestingController: HttpTestingController;
+  let router: Router;
+  let location: Location;
+
+  beforeEach(() => {
+    TestBed.overrideProvider(UserService, { useValue: new MockUserService() });
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        RouterTestingModule.withRoutes([
+          { path: 'users/1', component: UserProfileComponent }
+        ]),
+        HttpClientTestingModule
+      ],
+      declarations: [AddUserComponent, UserProfileComponent],
+    }).compileComponents().catch(error => {
+      expect(error).toBeNull();
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddUserComponent);
+    component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    // Set up the form with valid values.
+    // We don't actually have to do this, but it does mean that when we
+    // check that `submitForm()` is called with the right arguments below,
+    // we have some reason to believe that that wasn't passing "by accident".
+    component.addUserForm.controls.name.setValue('Chris Smith');
+    component.addUserForm.controls.age.setValue(27);
+    component.addUserForm.controls.company.setValue('Ohmnet');
+    component.addUserForm.controls.email.setValue('this@that.com');
+    component.addUserForm.controls.role.setValue('admin');
+  });
+
+  // The `fakeAsync()` wrapper is necessary because the `submitForm()` method
+  // calls `navigate()` on the router, which is an asynchronous operation, and we
+  // need to wait (using `tick()`) for that to complete before we can check the
+  // new location.
+  it('should call addUser() and handle success response', fakeAsync(() => {
+    // This use of `fixture.ngZone.run()` is necessary to avoid a warning when
+    // we run the tests. `submitForm()` calls `.navigate()` when it succeeds,
+    // and that apparently needs to be run in a separate Angular zone (a concept
+    // I don't claim to understand well). The suggestion in this lengthy
+    // thread: https://github.com/angular/angular/issues/25837
+    // is to wrap the relevant part of the test in an Angular zone, and that
+    // does seem to resolve the issue. Some people seem to feel that this is
+    // actually a workaround for a bug in Angular, but I'm not clear enough
+    // on the issues to know if that's true or not. - Nic
+    fixture.ngZone.run(() => {
+      // "Spy" on the `.addUser()` method in the user service. Here we basically
+      // intercept any calls to that method and return a canned response ('1').
+      // This means we don't have to worry about the details of the `.addUser()`,
+      // or actually have a server running to receive the HTTP request that
+      // `.addUser()` would typically generate. Note also that the particular values
+      // we set up in our form (e.g., 'Chris Smith') are actually ignored
+      // thanks to our `spyOn()` call.
+      const addUserSpy = spyOn(userService, 'addUser').and.returnValue(of('1'));
+      component.submitForm();
+      // Check that `.addUser()` was called with the form's values which we set
+      // up above.
+      expect(addUserSpy).toHaveBeenCalledWith(component.addUserForm.value);
+      // Wait for the router to navigate to the new page. This is necessary since
+      // navigation is an asynchronous operation.
+      tick();
+      // Now we can check that the router actually navigated to the right place.
+      expect(location.path()).toBe('/users/1');
+      // Flush any pending microtasks. This is necessary to ensure that the
+      // timer generated by `fakeAsync()` completes before the test finishes.
+      flush();
+    });
+  }));
+
+  // This doesn't need `fakeAsync()`, `tick()`, or `flush() because the
+  // error case doesn't navigate to another page. It just displays an error
+  // message in the snackbar. So, we don't need to worry about the asynchronous
+  // nature of navigation.
+  it('should call addUser() and handle error response', () => {
+    // Save the original path so we can check that it doesn't change.
+    let path = location.path();
+    // A canned error response to be returned by the spy.
+    const errorResponse = { status: 500, message: 'Server error' };
+    // "Spy" on the `.addUser()` method in the user service. Here we basically
+    // intercept any calls to that method and return the error response
+    // defined above.
+    const addUserSpy = spyOn(userService, 'addUser')
+      .and
+      .returnValue(throwError(() => errorResponse));
+    component.submitForm();
+    // Check that `.addUser()` was called with the form's values which we set
+    // up above.
+    expect(addUserSpy).toHaveBeenCalledWith(component.addUserForm.value);
+    // Confirm that we're still at the same path.
+    expect(location.path()).toBe(path);
+  });
 });
